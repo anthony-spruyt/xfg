ea376ff48aad63ef336313b8b5d64d5b7445071d
Anthony Spruyt <aspruyt@hotmail.co.uk>
2026-02-11 12:17:17 +0000

    refactor(settings): remove unused resource-converters.ts

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -3,6 +3,3 @@ export * from "./rulesets/index.js";
 
 // Repo settings
 export * from "./repo-settings/index.js";
-
-// Resource converters
-export { syncResultToResources } from "./resource-converters.js";
